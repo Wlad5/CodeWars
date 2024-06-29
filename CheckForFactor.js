7e00899898642c1f8ba20abccd91b33f7f7bd8c2
Wlad5 <cebotarivlad814@gmail.com>
2024-06-29 10:19:48 +0200

    Add check for factor.

---
+++ CheckForFactor.js
@@ -0,0 +1,14 @@
+function checkForFactor (base, factor) {
+  
+    const remainder = base % factor;
+    
+    if (remainder === 0) {
+      
+      return true;
+    
+    } else {
+    
+      return false;
+    
+    }
+  }
